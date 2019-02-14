9d4aeec054d538db9ee7a9e3cf899dd4548f8f2c
jwfrasier <frasier.joseph@gmail.com>
2019-02-14 10:12:16 -0600

    first commit, working on displaying data

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>This is header</p>
+      </div>
+    );
+  }
+}
